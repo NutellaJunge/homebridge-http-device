6bab26891902c0becf2b0eaac4f8cfd49d1a787f
NutellaJunge <52011439+NutellaJunge@users.noreply.github.com>
2020-12-19 13:38:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,49 @@ module.exports = function (homebridge) {
   Characteristic = homebridge.hap.Characteristic;
   homebridge.registerAccessory("switch-plugin", "MyAwesomeSwitch", mySwitch);
 };
+
+const request = require('request');
+const url = require('url');
+
+function mySwitch(log, config) {
+  this.log = log;
+  this.getUrl = url.parse(config['getUrl']);
+  this.postUrl = url.parse(config['postUrl']);
+}
+
+mySwitch.prototype = {
+
+  getSwitchOnCharacteristic: function (next) {
+    const me = this;
+    request({
+      url: me.getUrl,
+      method: 'GET',
+    },
+    function (error, response, body) {
+      if (error) {
+        me.log('STATUS: ' + response.statusCode);
+        me.log(error.message);
+        return next(error);
+      }
+      return next(null, body.currentState);
+    });
+  },
+
+  setSwitchOnCharacteristic: function (on, next) {
+    const me = this;
+    request({
+      url: me.postUrl,
+      body: {'targetState': on},
+      method: 'POST',
+      headers: {'Content-type': 'application/json'}
+    },
+    function (error, response) {
+      if (error) {
+        me.log('STATUS: ' + response.statusCode);
+        me.log(error.message);
+        return next(error);
+      }
+      return next();
+    });
+  }
+};
