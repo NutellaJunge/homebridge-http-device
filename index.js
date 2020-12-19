c752b11507a8831d5416eccbd7668a45a68fc5af
NutellaJunge <52011439+NutellaJunge@users.noreply.github.com>
2020-12-19 14:46:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ module.exports = function (homebridge) {
   homebridge.registerAccessory("switch-plugin", "MyAwesomeSwitch", mySwitch);
 };
 
+const request = require('request');
+const url = require('url');
+
+function mySwitch(log, config) {
+  this.log = log;
+  this.getUrl = url.parse(config['getUrl']);
+  this.postUrl = url.parse(config['postUrl']);
+}
+
 mySwitch.prototype.getServices = function () {
   let informationService = new Service.AccessoryInformation();
   informationService
@@ -24,48 +33,36 @@ mySwitch.prototype.getServices = function () {
   return [informationService, switchService];
 }
 
-const request = require('request');
-const url = require('url');
-
-function mySwitch(log, config) {
-  this.log = log;
-  this.getUrl = url.parse(config['getUrl']);
-  this.postUrl = url.parse(config['postUrl']);
+mySwitch.prototype.getSwitchOnCharacteristic = function (next) {
+  const me = this;
+  request({
+    url: me.getUrl,
+    method: 'GET',
+  },
+  function (error, response, body) {
+    if (error) {
+      me.log('STATUS: ' + response.statusCode);
+      me.log(error.message);
+      return next(error);
+    }
+    return next(null, body.currentState);
+  });
 }
 
-mySwitch.prototype = {
-
-  getSwitchOnCharacteristic: function (next) {
-    const me = this;
-    request({
-      url: me.getUrl,
-      method: 'GET',
-    },
-    function (error, response, body) {
-      if (error) {
-        me.log('STATUS: ' + response.statusCode);
-        me.log(error.message);
-        return next(error);
-      }
-      return next(null, body.currentState);
-    });
+mySwitch.prototype.setSwitchOnCharacteristic = function (on, next) {
+  const me = this;
+  request({
+    url: me.postUrl,
+    body: {'targetState': on},
+    method: 'POST',
+    headers: {'Content-type': 'application/json'}
   },
-
-  setSwitchOnCharacteristic: function (on, next) {
-    const me = this;
-    request({
-      url: me.postUrl,
-      body: {'targetState': on},
-      method: 'POST',
-      headers: {'Content-type': 'application/json'}
-    },
-    function (error, response) {
-      if (error) {
-        me.log('STATUS: ' + response.statusCode);
-        me.log(error.message);
-        return next(error);
-      }
-      return next();
-    });
-  }
-};
+  function (error, response) {
+    if (error) {
+      me.log('STATUS: ' + response.statusCode);
+      me.log(error.message);
+      return next(error);
+    }
+    return next();
+  });
+}
