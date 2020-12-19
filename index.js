09e72c39c7bc0e899538710acaf1405c98a961d5
NutellaJunge <52011439+NutellaJunge@users.noreply.github.com>
2020-12-19 15:02:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ mySwitch.prototype.getSwitchOnCharacteristic = function (next) {
       me.log(error.message);
       return next(error);
     }
-    me.log(body.currentState);
+    me.log(body);
     return next(null, body.currentState);
   });
 }
